5169bfdbd8abe142951ede76aff01206365f4438
MacRat <m@crat.jp>
2019-11-04 22:24:05 +0900

    Added HTML headers

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,13 @@ dotenv.config();
 
 
 export default {
+    head: {
+        title: 'time management',
+        meta: [
+            {charset: 'utf-8'},
+            {name: 'viewport', content: 'width=device-width,initial-scale=1'},
+        ],
+    },
     modules: ['nuxt-client-init-module', 'cookie-universal-nuxt'],
     plugins: ['~/plugins/APIClient'],
     serverMiddleware: ['~/api/'],
